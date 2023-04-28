7340620e1f43e6b73f4d5b6fb81bc5b5c8fb1c7f
nogayhusrev <nogayhusrev@gmail.com>
2023-04-28 20:53:34 +0300

    Ticketing App Rest part-2 swagger implementation and keycloak connection done

--- src/main/java/com/nogayhusrev/controller/UserController.java
+++ src/main/java/com/nogayhusrev/controller/UserController.java
@@ -4,6 +4,8 @@ package com.nogayhusrev.controller;
 import com.nogayhusrev.dto.ResponseWrapper;
 import com.nogayhusrev.dto.UserDTO;
 import com.nogayhusrev.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,52 +13,59 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/v1/user")
+@Tag(name = "UserController", description = "User API")
 public class UserController {
+
     private final UserService userService;
-
-
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-
-    @GetMapping()
-    @RolesAllowed({"Manager","Admin"})
-    public ResponseEntity<ResponseWrapper> getUsers(){
-
+    @GetMapping
+    @RolesAllowed({"Manager", "Admin"})
+    @Operation(summary = "Get users")
+    public ResponseEntity<ResponseWrapper> getUsers() {
         List<UserDTO> userDTOList = userService.listAllUsers();
-        return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieved",userDTOList, HttpStatus.OK));
-
+        return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieves", userDTOList, HttpStatus.OK));
     }
 
     @GetMapping("/{username}")
     @RolesAllowed("Admin")
-    public ResponseEntity<ResponseWrapper> getUserByName(@PathVariable("username") String username){
-        UserDTO user = userService.findByUserName(username);
-        return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved",user,HttpStatus.OK));
+    @Operation(summary = "Get user by username")
+    public ResponseEntity<ResponseWrapper> getUserByUserName(@PathVariable("username") String userName) {
+        UserDTO user = userService.findByUserName(userName);
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved", user, HttpStatus.OK));
     }
-    @PostMapping()
+
+    @PostMapping
     @RolesAllowed("Admin")
-    public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user){
+    @Operation(summary = "Create user")
+    public ResponseEntity<ResponseWrapper> createUser(@RequestBody UserDTO user) {
         userService.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User is successfully created",HttpStatus.CREATED));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("User is successfully created", HttpStatus.CREATED));
     }
+
     @PutMapping
     @RolesAllowed("Admin")
-    public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user){
-
+    @Operation(summary = "Update user")
+    public ResponseEntity<ResponseWrapper> updateUser(@RequestBody UserDTO user) {
         userService.update(user);
-        return ResponseEntity.ok(new ResponseWrapper("User is successfully created",HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully updated", HttpStatus.OK));
 
     }
+
     @DeleteMapping("/{username}")
     @RolesAllowed("Admin")
-    public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String username){
-        userService.delete(username);
-        return ResponseEntity.ok(new ResponseWrapper("User is successfully deleted",HttpStatus.OK));
+    @Operation(summary = "Delete user")
+    public ResponseEntity<ResponseWrapper> deleteUser(@PathVariable("username") String userName) {
+        userService.delete(userName);
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully deleted", HttpStatus.OK));
+
     }
+
 
 }
